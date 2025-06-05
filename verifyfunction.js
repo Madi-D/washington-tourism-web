c096c27691bde220d81a9b2bf88da839b7f3e255
Madi-D <63809763+Madi-D@users.noreply.github.com>
2025-06-05 09:36:00 -0700

    Add files via upload

---
+++ verifyfunction.js
@@ -0,0 +1,15 @@
+﻿function validateEmail(event) {
+   let Form = document.querySelector("#orm");
+
+    Form.email.style.backgroundColor = "LightGreen";
+    Form.verifyemail.style.backgroundColor = "LightGreen";
+
+  if (Form.email.value != Form.verifyemail.value) {
+      Form.email.style.backgroundColor = "LightRed";
+      Form.verifyemail.style.backgroundColor = "LightRed";
+  }
+  
+}
+
+let Form = document.querySelector("#Form");
+Form.validate.addEventListener("click", validateEmail);
